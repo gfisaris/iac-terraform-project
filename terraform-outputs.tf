ec3a9c0d7da19ed49aeb26ed05c5a627652ae577
gfisaris <gfisaris@gmail.com>
2016-11-19 11:26:37 +0000

    Add Terraform Outputs

---
+++ terraform-outputs.tf
@@ -0,0 +1,3 @@
+output "aws_region"     {value = "${var.tf_provider_aws_region}"}
+output "aws_access_key" {value = "${var.tf_provider_aws_access_key}"}
+output "aws_secret_key" {value = "${var.tf_provider_aws_secret_key}"}
--- terraform-variables.tf
+++ terraform-variables.tf
@@ -1,5 +1,5 @@
 ##  AWS Account Information  ##
 
-  variable "tf_provider_aws_region"     {description  = "AWS Region     "}
-  variable "tf_provider_aws_access_key" {description = "AWS Access Key "}
-  variable "tf_provider_aws_secret_key" {description  = "AWS Secret Key "}
+  variable "tf_provider_aws_region"     {description = "AWS Region"}
+  variable "tf_provider_aws_access_key" {description = "AWS Access Key"}
+  variable "tf_provider_aws_secret_key" {description = "AWS Secret Key"}
